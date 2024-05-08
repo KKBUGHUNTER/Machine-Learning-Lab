400c689105542f1ea2452f8725964c82fb9e0075
kkbughunter <karthikeyan.aas@gmail.com>
2024-05-08 11:36:09 +0530

    mini-project

---
+++ Mini Project/Server.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import pickle
+
+app = Flask(__name__)
+
+# Load the .pkl file
+def load_model(file_path):
+    with open(file_path, 'rb') as file:
+        model = pickle.load(file)
+    return model
+
+# Define the route to access the model
+@app.route('/model', methods=['GET'])
+def get_model():
+    model = load_model('Model.pkl')  # Replace 'example.pkl' with your file path
+    # Convert the model to JSON and return
+    return jsonify({"model": model})
+
+if __name__ == '__main__':
+    app.run(debug=True)
